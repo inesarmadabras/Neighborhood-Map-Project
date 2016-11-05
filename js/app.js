2d3be8bb8e40b680db4d971d6e7d52b90c6c30ed
Inês Armada Brás <inesarmadabras@users.noreply.github.com>
2016-11-05 02:56:54 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,115 @@
 //(1) Resubmission -  API keys are now at the top of the file
 //(2) Resubmission -  single-quotes (') over double-quotes (")
 var flickrKey = '04ae986d31f8a3d5993799e1d88c48c5';
+// Night mode map:
+// ref: https://developers.google.com/maps/documentation/javascript/examples/style-array
+var styles = [
+        {
+            elementType: 'geometry',
+            stylers: [{
+                color: '#242f3e'
+            }]
+        }, {
+            elementType: 'labels.text.stroke',
+            stylers: [{
+                color: '#242f3e'
+            }]
+        }, {
+            elementType: 'labels.text.fill',
+            stylers: [{
+                color: '746855'
+            }]
+        }, {
+            featureType: 'administrative.locality',
+            elementType: 'labels.text.fill',
+            stylers: [{
+                color: '#d59563'
+            }]
+        }, {
+            featureType: 'poi',
+            elementType: 'labels.text.fill',
+            stylers: [{
+                color: '#d59563'
+            }]
+        }, {
+            featureType: 'poi.park',
+            elementType: 'geometry',
+            stylers: [{
+                color: '#263c3f'
+            }]
+        }, {
+            featureType: 'poi.park',
+            elementType: 'labels.text.fill',
+            stylers: [{
+                color: '#6b9a76'
+            }]
+        }, {
+            featureType: 'road',
+            elementType: 'geometry',
+            stylers: [{
+                color: '#38414e'
+            }]
+        }, {
+            featureType: 'road',
+            elementType: 'geometry.stroke',
+            stylers: [{
+                color: '#212a37'
+            }]
+        }, {
+            featureType: 'road',
+            elementType: 'labels.text.fill',
+            stylers: [{
+                color: '#9ca5b3'
+            }]
+        }, {
+            featureType: 'road.highway',
+            elementType: 'geometry',
+            stylers: [{
+                color: '#746855'
+            }]
+        }, {
+            featureType: 'road.highway',
+            elementType: 'geometry.stroke',
+            stylers: [{
+                color: '#1f2835'
+            }]
+        }, {
+            featureType: 'road.highway',
+            elementType: 'labels.text.fill',
+            stylers: [{
+                color: '#f3d19c'
+            }]
+        }, {
+            featureType: 'transit',
+            elementType: 'geometry',
+            stylers: [{
+                color: '#2f3948'
+            }]
+        }, {
+            featureType: 'transit.station',
+            elementType: 'labels.text.fill',
+            stylers: [{
+                color: '#d59563'
+            }]
+        }, {
+            featureType: 'water',
+            elementType: 'geometry',
+            stylers: [{
+                color: '#17263c'
+            }]
+        }, {
+            featureType: 'water',
+            elementType: 'labels.text.fill',
+            stylers: [{
+                color: '#515c6d'
+            }]
+        }, {
+            featureType: 'water',
+            elementType: 'labels.text.stroke',
+            stylers: [{
+                color: '#17263c'
+            }]
+        }];
 var ViewModel = function() {
     var self = this;
     // (3) Resubmission - added new locations.
@@ -150,114 +259,7 @@ initMap = () => {
     // Initialize map
     var map = new google.maps.Map(mapDiv, {
         zoom: 13,
-        // Night mode map:
-        // ref: https://developers.google.com/maps/documentation/javascript/examples/style-array
-        styles: [{
-            elementType: 'geometry',
-            stylers: [{
-                color: '#242f3e'
-            }]
-        }, {
-            elementType: 'labels.text.stroke',
-            stylers: [{
-                color: '#242f3e'
-            }]
-        }, {
-            elementType: 'labels.text.fill',
-            stylers: [{
-                color: '746855'
-            }]
-        }, {
-            featureType: 'administrative.locality',
-            elementType: 'labels.text.fill',
-            stylers: [{
-                color: '#d59563'
-            }]
-        }, {
-            featureType: 'poi',
-            elementType: 'labels.text.fill',
-            stylers: [{
-                color: '#d59563'
-            }]
-        }, {
-            featureType: 'poi.park',
-            elementType: 'geometry',
-            stylers: [{
-                color: '#263c3f'
-            }]
-        }, {
-            featureType: 'poi.park',
-            elementType: 'labels.text.fill',
-            stylers: [{
-                color: '#6b9a76'
-            }]
-        }, {
-            featureType: 'road',
-            elementType: 'geometry',
-            stylers: [{
-                color: '#38414e'
-            }]
-        }, {
-            featureType: 'road',
-            elementType: 'geometry.stroke',
-            stylers: [{
-                color: '#212a37'
-            }]
-        }, {
-            featureType: 'road',
-            elementType: 'labels.text.fill',
-            stylers: [{
-                color: '#9ca5b3'
-            }]
-        }, {
-            featureType: 'road.highway',
-            elementType: 'geometry',
-            stylers: [{
-                color: '#746855'
-            }]
-        }, {
-            featureType: 'road.highway',
-            elementType: 'geometry.stroke',
-            stylers: [{
-                color: '#1f2835'
-            }]
-        }, {
-            featureType: 'road.highway',
-            elementType: 'labels.text.fill',
-            stylers: [{
-                color: '#f3d19c'
-            }]
-        }, {
-            featureType: 'transit',
-            elementType: 'geometry',
-            stylers: [{
-                color: '#2f3948'
-            }]
-        }, {
-            featureType: 'transit.station',
-            elementType: 'labels.text.fill',
-            stylers: [{
-                color: '#d59563'
-            }]
-        }, {
-            featureType: 'water',
-            elementType: 'geometry',
-            stylers: [{
-                color: '#17263c'
-            }]
-        }, {
-            featureType: 'water',
-            elementType: 'labels.text.fill',
-            stylers: [{
-                color: '#515c6d'
-            }]
-        }, {
-            featureType: 'water',
-            elementType: 'labels.text.stroke',
-            stylers: [{
-                color: '#17263c'
-            }]
-        }],
+        styles: styles,
         center: {
             lat: 38.6766764,
             lng: -9.184167
